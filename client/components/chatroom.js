da2201c74c9d2096c3bb75ef7744007ed246082b
Daniel Stracquadanio <daniel.stracquadanio@gmail.com>
2020-05-29 15:01:05 -0400

    Columns collapse in mobile view

--- client/components/chatroom.js
+++ client/components/chatroom.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Button, Container, Badge} from 'react-bootstrap'
+import {Button, Container, Badge, Row, Col} from 'react-bootstrap'
 import io from 'socket.io-client'
 const moment = require('moment')
 import {AiFillWechat, AiOutlineSend} from 'react-icons/ai'
@@ -81,120 +81,120 @@ export default class Chatroom extends Component {
 
   render() {
     return (
-      <div>
-        <div className="chat-container">
-          <header className="chat-header">
-            <h1>Welcome to {this.state.room.name}!</h1>
-            <br />
+      <Container className="chat-container">
+        <Row className="chat-header">
+          <h1>Welcome to {this.state.room.name}!</h1>
+          <br />
+          <h1>
+            <i>
+              <AiFillWechat /> Lobby
+            </i>
+          </h1>
+        </Row>
+        <Row className="chat-main">
+          <Col md={4} className="chat-sidebar">
+            <h1>Users in Lobby:</h1>
+            <ul>
+              {this.props.inRoom.map((user) => {
+                return <h2 key={user.id}>{user.name}</h2>
+              })}
+            </ul>
             <h1>
-              <i>
-                <AiFillWechat /> Lobby
-              </i>
+              Current Artist:
+              {this.props.user.name === this.props.currentArtist.name
+                ? ' YOU!'
+                : ' ' + this.props.currentArtist.name}
             </h1>
-          </header>
-          <main className="chat-main">
-            <div className="chat-sidebar">
-              <h1>Users in Lobby:</h1>
-              <ul>
-                {this.props.inRoom.map((user) => {
-                  return <h2 key={user.id}>{user.name}</h2>
-                })}
-              </ul>
-              <h1>
-                Current Artist:
-                {this.props.user.name === this.props.currentArtist.name
-                  ? ' YOU!'
-                  : ' ' + this.props.currentArtist.name}
-              </h1>
-              {this.props.user.name === this.props.currentArtist.name ? (
+            {this.props.user.name === this.props.currentArtist.name ? (
+              <Button
+                type="button"
+                variant="light"
+                onClick={this.props.handlePass}
+                className="sidebar-button"
+              >
+                Pass The Paintbrush
+              </Button>
+            ) : null}
+            {this.props.user.name === this.props.currentArtist.name ? (
+              <div>
+                <h1>Your word is: {this.props.gameWord.toUpperCase()} </h1>
                 <Button
                   type="button"
                   variant="light"
-                  onClick={this.props.handlePass}
-                  className="artist-button"
+                  onClick={this.props.wordGenerator}
+                  className="sidebar-button"
                 >
-                  Pass The Paintbrush
+                  Regenerate Word
                 </Button>
-              ) : null}
-              {this.props.user.name === this.props.currentArtist.name ? (
-                <div>
-                  <h1>Your word is: {this.props.gameWord.toUpperCase()} </h1>
-                  <Button
-                    type="button"
-                    variant="light"
-                    onClick={this.props.wordGenerator}
-                    className="artist-button"
-                  >
-                    Regenerate Word
-                  </Button>
-                </div>
-              ) : (
-                <div>
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <h4>
-                    <Badge variant="light">
-                      Waiting for Artist to start game
-                    </Badge>
-                  </h4>
-                </div>
-              )}
-            </div>
-            <div className="chat-messages">
-              {this.state.messages.map((message, index) => (
-                <div className="message" key={index}>
-                  <p className="meta">
-                    {message.name} <span>{message.timestamp}</span>
-                  </p>
-                  <p className="text">{message.message}</p>
-                </div>
-              ))}
-              <div
-                style={{float: 'left', clear: 'both'}}
-                ref={(el) => {
-                  this.messagesEnd = el
-                }}
-              />
-            </div>
-          </main>
-          <div className="chat-form-container">
-            <form id="chat-form" onSubmit={this.handleSubmit}>
-              <Button
-                type="button"
-                variant="danger"
-                onClick={this.props.handleClick}
-                className="chat-button"
-              >
-                Leave Lobby
-              </Button>
-              <input
-                name="message"
-                type="text"
-                placeholder="Enter Message"
-                value={this.state.message}
-                onChange={this.handleChange}
-              />
-              <Button type="submit" variant="primary" className="chat-button">
-                Send <AiOutlineSend />
-              </Button>
-            </form>
-          </div>
-        </div>
+              </div>
+            ) : (
+              <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <h4>
+                  <Badge variant="light">
+                    Waiting for Artist to start game
+                  </Badge>
+                </h4>
+              </div>
+            )}
+          </Col>
+          <Col md={8} className="chat-messages">
+            {this.state.messages.map((message, index) => (
+              <div className="message" key={index}>
+                <p className="meta">
+                  {message.name} <span>{message.timestamp}</span>
+                </p>
+                <p className="text">{message.message}</p>
+              </div>
+            ))}
+            <div
+              style={{float: 'left', clear: 'both'}}
+              ref={(el) => {
+                this.messagesEnd = el
+              }}
+            />
+          </Col>
+        </Row>
+        <Row className="chat-form-container">
+          <form id="chat-form" onSubmit={this.handleSubmit}>
+            <Button
+              type="button"
+              variant="danger"
+              onClick={this.props.handleClick}
+              className="chat-button"
+            >
+              Leave Lobby
+            </Button>
+            <input
+              name="message"
+              type="text"
+              placeholder="Enter Message"
+              value={this.state.message}
+              onChange={this.handleChange}
+            />
+            <Button type="submit" variant="primary" className="chat-button">
+              Send <AiOutlineSend />
+            </Button>
+          </form>
+        </Row>
         {this.props.user.name === this.props.currentArtist.name ? (
-          <Button
-            type="button"
-            className="start-button"
-            variant="success"
-            onClick={this.props.startGame}
-            size="lg"
-            block
-          >
-            <span className="start-game">S T A R T G A M E !</span>
-          </Button>
+          <Row>
+            <Button
+              type="button"
+              className="start-button"
+              variant="success"
+              onClick={this.props.startGame}
+              size="lg"
+              block
+            >
+              <span className="start-game">S T A R T G A M E !</span>
+            </Button>
+          </Row>
         ) : null}
-      </div>
+      </Container>
     )
   }
 }
